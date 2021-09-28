f6d60758b82acafdd0c08cbd9fd2dfcd3321e3d5
babek <student@24-02>
2021-09-28 12:30:52 +0300

    init commit

---
+++ ConsoleApp2/Client.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Client
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            Client1 client = new Client1();
+            try
+            {
+                client.Connect();
+                while (true)
+                {
+
+                    Console.WriteLine(client.GetMsg().ToString());
+
+                }
+
+
+                //client.Disconnect();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Client;
+using System.Collections.Generic;
+
+namespace Server
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            Server1 server = new Server1();
+            server.StartServer();
+            Task.Factory.StartNew(() => server.Connects());
+            try
+            {
+                while (true)
+                {
+                    server.SendMsg(Console.ReadLine());
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+                throw;
+            }
+        }
+    }
+}
